1af55587a3d5be84d8d1061e88d523a18dd1b2da
Grayson Cox <GraysonCox98@gmail.com>
2019-11-07 13:17:34 -0600

    rbtree is done except for delete_node

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,7 @@
 #include <iostream>
 #include <io/file_writer.h>
 #include "io/file_reader.h"
+#include <model/rbtree.h>
 
 using namespace std;
 
@@ -17,19 +18,54 @@ typedef enum {
 	UNKNOWN
 } operation_t;
 
-int main() {
-	file_reader f(INPUT_FILE);
-	if (!f.is_open()) {
-		return 1;
+void insert_test(initializer_list<int> keys) {
+	rbtree t;
+	for (auto k : keys) {
+		cout << "Inserting " << k << " ..." << endl;
+		t.insert_node(k);
+		cout << t.to_string() << endl;
 	}
-	while (!f.is_at_end()) {
-		cout << f.read_line() << endl;
+	for (auto k : keys) {
+		int x = k + random() % 2;
+		cout << "Searching for " << x << " ... ";
+		if (t.search(x)) {
+			cout << "found" << endl;
+		} else {
+			cout << "not found" << endl;
+		}
 	}
+}
 
-	file_writer fileWriter("/Users/administrator/Documents/ISU/COMS352/PA2/Grayson_Cox_Project2/src/dong.txt");
-	for (int i = 0; i < 10; i++) {
-		fileWriter.write_line("This is text.");
+template<typename T>
+vector<T> to_vector(initializer_list<T> list) {
+	vector<T> v;
+	for (auto t : list) {
+		v.push_back(t);
 	}
+	return v;
+}
+
+//
+// 45b,1b,f,34r,f,f,97b,f,110r,f,f
+//
+
+int main() {
+	initializer_list<rbnode *> nodes = {
+			new rbnode(45, BLACK),
+			new rbnode(1, BLACK),
+			nullptr,
+			new rbnode(34, RED),
+			nullptr,
+			nullptr,
+			new rbnode(97, BLACK),
+			nullptr,
+			new rbnode(110, RED),
+			nullptr,
+			nullptr
+	};
+	rbtree t(to_vector(nodes));
+
+	cout << t.to_string() << endl;
 
 	return 0;
 }
---
+++ src/model/rbnode.cpp
@@ -0,0 +1,42 @@
+//
+// Created by Grayson Cox on 11/20/19.
+//
+
+#include "model/rbnode.h"
+
+rbnode::rbnode(int key, rbnode_color color) : key(key), color(color) {
+	left = nullptr;
+	right = nullptr;
+}
+
+int rbnode::get_key() const {
+	return key;
+}
+
+void rbnode::set_key(int key) {
+	rbnode::key = key;
+}
+
+rbnode_color rbnode::get_color() const {
+	return color;
+}
+
+void rbnode::set_color(rbnode_color color) {
+	rbnode::color = color;
+}
+
+rbnode *rbnode::get_left() const {
+	return left;
+}
+
+void rbnode::set_left(rbnode *n) {
+	rbnode::left = n;
+}
+
+rbnode *rbnode::get_right() const {
+	return right;
+}
+
+void rbnode::set_right(rbnode *n) {
+	rbnode::right = n;
+}

99f94e2b1a0299426531b607771584f0d402b423
Grayson Cox <GraysonCox98@gmail.com>
2019-12-05 14:11:19 -0600

    Add documentation, add mutex variable for write_line()

--- src/io/file_writer.cpp
+++ src/io/file_writer.cpp
@@ -1,19 +1,24 @@
 //
-// Created by Grayson on 11/7/19.
+// Created by Grayson Cox on 11/7/19.
 //
 
 #include "io/file_writer.h"
 
-file_writer::file_writer(std::string file) {
-    output_file.open(file);
+file_writer::file_writer(const std::string &file) {
+	output_file = new std::ofstream(file);
+	pthread_mutex_init(&m, nullptr);
 }
 
 file_writer::~file_writer() {
-    if (output_file.is_open()) {
-        output_file.close();
-    }
+	pthread_mutex_destroy(&m);
+	if (output_file->is_open()) {
+		output_file->close();
+	}
+	delete output_file;
 }
 
-void file_writer::write_line(std::string str) {
-    output_file << str << std::endl;
+void file_writer::write_line(const std::string &str) {
+	pthread_mutex_lock(&m);
+	*output_file << str << std::endl;
+	pthread_mutex_unlock(&m);
 }

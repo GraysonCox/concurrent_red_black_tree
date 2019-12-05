e49ca57c42d39a58cdaed1f31316eaaeca1a8fc4
Grayson Cox <GraysonCox98@gmail.com>
2019-12-05 13:51:53 -0600

    Remove unused methods, add documentation

--- include/io/file_reader.h
+++ include/io/file_reader.h
@@ -2,36 +2,71 @@
 // Created by Grayson Cox on 11/7/19.
 //
 
-#ifndef CONCURRENT_RED_BLACK_TREE_FILE_READER_H
-#define CONCURRENT_RED_BLACK_TREE_FILE_READER_H
+#ifndef FILE_READER_H
+#define FILE_READER_H
 
 
 #include <string>
 #include <fstream>
 #include <iostream>
 
+/**
+ * A class that reads from text files. This seemed like a good idea
+ * when I made it.
+ */
 class file_reader {
 public:
 
-	file_reader(std::string file);
+	/**
+	 * Initializes a file_reader for the given file.
+	 *
+	 * @param file - Name of file to read.
+	 */
+	explicit file_reader(const std::string &file);
 
+	/**
+	 * Default destructor that closes the input file and
+	 * what not.
+	 */
 	virtual ~file_reader();
 
+	/**
+	 * Reads and returns all text from the input file up to
+	 * the next newline character ('\n').
+	 *
+	 * @return A string containing all text from the input file up to
+	 * 			the next newline character ('\n').
+	 */
 	std::string read_line();
 
+	/**
+	 * Reads and returns all text from the input file up to the next
+	 * white space or newline character. This method ignores preceding
+	 * spaces.
+	 *
+	 * @return A string containing the next token in the file.
+	 */
 	std::string read_token();
 
-	std::string read_to(char delimiter);
-
+	/**
+	 * Returns true if the input file was opened successfully.
+	 *
+	 * @return true if the input file was opened successfully.
+	 */
 	bool is_open();
 
+	/**
+	 * Returns true if the input file has no more text to read.
+	 *
+	 * @return true if the input file has no more text to read.
+	 */
 	bool is_end_of_file();
 
-	bool is_end_of_line();
-
 private:
-	std::ifstream input_file;
+
+	std::ifstream *input_file;
+
 };
 
 
-#endif //CONCURRENT_RED_BLACK_TREE_FILE_READER_H
+#endif //FILE_READER_H
--- src/io/file_reader.cpp
+++ src/io/file_reader.cpp
@@ -4,51 +4,36 @@
 
 #include "io/file_reader.h"
 
-file_reader::file_reader(std::string file) {
-	input_file.open(file);
+file_reader::file_reader(const std::string &file) {
+	input_file = new std::ifstream(file);
 }
 
 file_reader::~file_reader() {
-	if (input_file.is_open()) {
-		input_file.close();
+	if (input_file->is_open()) {
+		input_file->close();
 	}
+	delete input_file;
 }
 
 std::string file_reader::read_line() {
 	std::string str;
-	getline(input_file, str);
+	getline(*input_file, str);
 	return str;
 }
 
 std::string file_reader::read_token() {
-	while (input_file.peek() == ' ') {
-		input_file.ignore();
+	while (input_file->peek() == ' ') {
+		input_file->ignore();
 	}
 	std::string str;
-	input_file >> str;
-	return str;
-}
-
-std::string file_reader::read_to(char delimiter) {
-	while (input_file.peek() == ' ') {
-		input_file.ignore();
-	}
-	std::string str;
-	getline(input_file, str, delimiter);
+	*input_file >> str;
 	return str;
 }
 
 bool file_reader::is_open() {
-	return input_file.is_open();
+	return input_file->is_open();
 }
 
 bool file_reader::is_end_of_file() {
-	return input_file.eof();
-}
-
-bool file_reader::is_end_of_line() {
-	char i = input_file.peek();
-	char j = '\n';
-	bool k = i == j;
-	return k;
+	return input_file->eof();
 }
